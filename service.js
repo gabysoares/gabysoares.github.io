29ee740cd271cba1c9b4cecb01a1fec24777820a
YOUR NAME <gabriella.soares03@gmail.com>
2019-05-05 10:22:59 +1000

    fixed up password to mailing list flow. also removed mailing list button from navbar.

--- service.js
+++ service.js
@@ -1,24 +1,11 @@
-// var password = "test";
-
-// (function password() {
-// 	var psw = prompt("Enter your Password");
-// 	while (psw != password) {
-//     alert("Incorrect Password");
-//     return password();
-// 	}
-// }());
-
-// alert('Enter')
-
-
 function myFunction() {
 	var password = "test123";
 	var finalpassword = password.toLowerCase();
-  userpwd = prompt("password ('test123 for now')";
+  userpwd = prompt("password ('test123 for now')");
   if (userpwd !== finalpassword) {
-  	alert("repent")
-  	// window.location.href = "index.html";
-  } else {
+  	alert("no")
+  	window.location.href = "index.html";
+  } if (userpwd == finalpassword) {
   	alert("welcome")
   	window.location.href = "info.html";
   }
