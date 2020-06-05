8480b6ad17e56c2d924e69aae9579118365bb970
YOUR NAME <gabriella.soares03@gmail.com>
2020-06-05 12:07:54 +1000

    First commit
    - Home page built
    - OAuth working (request URL good)
    
    —
    - Need to build user request
    - Check mobile

--- service.js
+++ service.js
@@ -1,12 +1,30 @@
 function myFunction() {
-	var password = "test123";
-	var finalpassword = password.toLowerCase();
-  userpwd = prompt("password ('test123 for now')");
-  if (userpwd !== finalpassword) {
-  	alert("no")
-  	window.location.href = "index.html";
-  } if (userpwd == finalpassword) {
-  	alert("welcome")
-  	window.location.href = "info.html";
-  }
+var client_id = 'abf07e6e514347eb8e09018fb199192a'
+popup = window.open(
+  "https://accounts.spotify.com/authorize?client_id=abf07e6e514347eb8e09018fb199192a&response_type=code&redirect_uri=https://developer.spotify.com/documentation/general/guides/authorization-guide/&scope=playlist-modify-public%20playlist-modify-private%20user-top-read",
+  'Login with Spotify',
+  'width=800,height=600'
+)
+
+window.spotifyCallback = (payload) => {
+  popup.close()
+
+  fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      'Authorization': `Bearer ${payload}`
+    }
+  }).then(response => {
+    return response.json()
+  }).then(data => {
+    // do something with data
+  })
 }
+
+token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+
+if (token) {
+  window.opener.spotifyCallback(token)
+}
+
+}
+
