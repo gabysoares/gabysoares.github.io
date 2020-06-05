8fe0b5972e8093f47ddbec92876d66edadaf184a
YOUR NAME <gabriella.soares03@gmail.com>
2020-06-05 22:26:38 +1000

    Implicit Auth Flow
    Testing new auth flow and getting access token to get top artists

--- service.js
+++ service.js
@@ -1,30 +1,30 @@
 function myFunction() {
-var client_id = 'abf07e6e514347eb8e09018fb199192a'
-popup = window.open(
-  "https://accounts.spotify.com/authorize?client_id=abf07e6e514347eb8e09018fb199192a&response_type=code&redirect_uri=https://developer.spotify.com/documentation/general/guides/authorization-guide/&scope=playlist-modify-public%20playlist-modify-private%20user-top-read",
-  'Login with Spotify',
-  'width=800,height=600'
-)
+// var client_id = 'abf07e6e514347eb8e09018fb199192a'
+// // var popup = window.open(
+// //   "https://accounts.spotify.com/authorize?client_id=abf07e6e514347eb8e09018fb199192a&response_type=code&redirect_uri=https://developer.spotify.com/documentation/general/guides/authorization-guide/&scope=playlist-modify-public%20playlist-modify-private%20user-top-read",
+// //   'Login with Spotify',
+// //   'width=800,height=600'
+// // )
 
-window.spotifyCallback = (payload) => {
-  popup.close()
+location.href = "https://accounts.spotify.com/authorize?client_id=abf07e6e514347eb8e09018fb199192a&response_type=code&redirect_uri=https://gabysoares.github.io/&scope=playlist-modify-public%20playlist-modify-private%20user-top-read";
 
-  fetch('https://api.spotify.com/v1/me', {
-    headers: {
-      'Authorization': `Bearer ${payload}`
-    }
-  }).then(response => {
-    return response.json()
-  }).then(data => {
-    // do something with data
-  })
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+console.log(_token);
+
 }
 
-token = window.location.hash.substr(1).split('&')[0].split("=")[1]
-
-if (token) {
-  window.opener.spotifyCallback(token)
-}
-
-}
 
