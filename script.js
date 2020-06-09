bb68d628b4c7bc51b19d7a5dcbe651fae1e2b23f
YOUR NAME <gabriella.soares03@gmail.com>
2020-06-09 13:14:39 +1000

    I think I get it

--- script.js
+++ script.js
@@ -34,12 +34,9 @@ const scopes = [
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+  getTracks(_token);
 } 
 
-if (_token){
-    getTracks(_token);
-
-} 
 
 }
 // Make a call using the token
