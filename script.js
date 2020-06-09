488072b7039ab318bb1a44612755765248a5e501
YOUR NAME <gabriella.soares03@gmail.com>
2020-06-09 19:44:56 +1000

    Working

--- script.js
+++ script.js
@@ -2,22 +2,6 @@
 
 function myFunction(){
 
-if (_token){
-
-  $.ajax({
-   url: "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50",
-   type: "GET",
-   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-   success: function(data) { 
-     // Do something with the returned data
-     data.items.map(function(track) {
-       let item = $('<li>' + track.name + ' - ' +track.artists[0] + '</li>');
-       item.appendTo($('#top-tracks'));
-     });
-   }
-});
-
-}
 
 console.log("test");
 
@@ -50,6 +34,15 @@ const scopes = [
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+}
+ 
+console.log("here");
+getTracks(_token);
+
+}
+// Make a call using the token
+
+function getTracks(_token){
 $.ajax({
    url: "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50",
    type: "GET",
@@ -57,14 +50,12 @@ $.ajax({
    success: function(data) { 
      // Do something with the returned data
      data.items.map(function(track) {
-       let item = $('<li>' + track.name + ' - ' +track.artists[0] + '</li>');
+       let item = $('<li>' + track.name + '</li>');
        item.appendTo($('#top-tracks'));
      });
    }
 });
-} 
 
-// Make a call using the token
-
+console.log(_token);
 
 }
