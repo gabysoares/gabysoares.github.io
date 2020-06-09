2845d03cbad238146da99543354cd62f773cf90d
YOUR NAME <gabriella.soares03@gmail.com>
2020-06-09 13:35:58 +1000

    who knows

--- script.js
+++ script.js
@@ -50,7 +50,18 @@ const scopes = [
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
-
+$.ajax({
+   url: "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50",
+   type: "GET",
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+   success: function(data) { 
+     // Do something with the returned data
+     data.items.map(function(track) {
+       let item = $('<li>' + track.name + ' - ' +track.artists[0] + '</li>');
+       item.appendTo($('#top-tracks'));
+     });
+   }
+});
 } 
 
 // Make a call using the token
